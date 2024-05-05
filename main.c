0233c8aae6f920b85f93d78cb8946aa8d41e5f4f
TienLe98 <letiena149star@gmail.com>
2024-05-05 21:01:54 +0700

    test git graph

--- main.c
+++ main.c
@@ -11,19 +11,30 @@ int AddValue(int a, int b, int *p)
     *p = a;
     return(a + b);
 }
+
+int AddValue2(int *a, int b)
+{
+    *a = 50;
+    return (*a+b);
+}
+
+    
 int main()
 {
     int a =  10;
     int b = 15;
     int result;
+    int result_2;
     int *p;
     p = &a;
-
     result = AddValue(a, b, p);
 
+    result_2 = AddValue2(&a, b);
     printf("%d \n", result);
+    printf("%d \n", result_2);
 
+    printf("This is test git graph code");
     printf("The result is");
-    printf("%d \n", a);
+    printf("The value of a after = %d \n", a);
     return 0;
 }
